b8ff4b1b92e30aeedd2711f1b66bc71b7275e313
tclewis91 <tclewis91@gmail.com>
2015-06-23 11:59:46 -0500

    added buttons to navigation

--- app/assets/javascripts/navigation.js.jsx
+++ app/assets/javascripts/navigation.js.jsx
@@ -3,9 +3,12 @@ var Navigation = React.createClass({
   render: function () {
     return (
       <nav>
-      <p>Home</p>
-      <p>Posts</p>
+      <p onClick={this.homeClicked} className="btun btn-primary" <p>Home</p>
+      <p onClick={this.homeClicked} className="btun btn-primary"<p>Posts</p>
       </nav>
       );
+  },
+  homeClicked: function() {
+    window.location.href = '/';
   }
 });
