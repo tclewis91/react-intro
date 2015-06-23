a664ee689b0b2f7893fd9e73f3b79395711058c8
tclewis91 <tclewis91@gmail.com>
2015-06-23 12:10:52 -0500

    postsclicked and navigate functions added

--- app/assets/javascripts/navigation.js.jsx
+++ app/assets/javascripts/navigation.js.jsx
@@ -3,12 +3,20 @@ var Navigation = React.createClass({
   render: function () {
     return (
       <nav>
-      <p onClick={this.homeClicked} className="btun btn-primary" <p>Home</p>
-      <p onClick={this.homeClicked} className="btun btn-primary"<p>Posts</p>
+      <p onClick={this.homeClicked} className="btn btn-primary">Home</p>
+      <p onClick={this.homeClicked} className="btn btn-primary">Posts</p>
       </nav>
       );
   },
-  homeClicked: function() {
+  homeClicked: function () {
     window.location.href = '/';
+  },
+  postsClicked: function () {
+    this.navigate('/posts');
+  },
+
+  navigate: function(url) {
+    window.location.href = url;
   }
+
 });
